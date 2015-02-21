01e133ab5c227a6280cf8f87f67ce5139bba2b19
Tom Rosier <thr2@aber.ac.uk>
2015-02-21 17:00:35 +0000

    Commented index.js

--- index.js
+++ index.js
@@ -1,32 +1,44 @@
+/*
+ * This is the main execution point of the application.
+ */
 
+// Import all the other classes that have been created.
 var pCLI   = require('./playercli');
 var fCLI   = require('./framecli');
 var sBoard = require('./scoreboard');
 
+// Constant defining how many frames there are in the application.
 var LAST_FRAME_NUMBER = 10;
 
+// Method that is executed when the program is started.
 function start() {
-	
-	console.log('Starting Bowling application.');
 
+	// Get list of players from the CLI class.
 	var players = pCLI.getPlayers();	
 
+	// Start the game going through all the frames.
 	for (var i = 0; i < LAST_FRAME_NUMBER; i++) {
 
+		// Get the frame number that we are currently on.
 		var frameNumber = i + 1;
 
+		// Work through the players to give them there turns on the current frame.
 		for (var playersName in players) {
+
+			// Get the current player.
 			var player = players[playersName];
 
+			// Get the info for the current frame the player is playing.
 			var frame = fCLI.frameDialog(player.name, frameNumber);
 
+			// Add that frame to the player so we can reference it later.
 			player.addToFrames(frame);
 		}
 	}
 
+	// Once the game is over we pass the info to the scoreboard class that prints the results.
 	sBoard.print(players);
 
-	console.log('Exiting application.');
 }
 
 start();
